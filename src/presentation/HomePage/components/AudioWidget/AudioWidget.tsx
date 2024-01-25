2b5a442bca2365d8a966bebff64b8743fea9369e
Justin Heath <just.coder@outlook.com>
2024-01-25 21:03:32 +0000

    refactor: audiowidget complete

--- src/presentation/HomePage/components/AudioWidget/AudioWidget.tsx
+++ src/presentation/HomePage/components/AudioWidget/AudioWidget.tsx
@@ -3,20 +3,23 @@ import { Button } from "@mui/material";
 import React, { useId, useRef, useState } from 'react';
 import sound from '../../../../assets/HomePage.mp3';
 
+import './AudioWidget.css'
+
 const AudioWidget: React.FC = () => {
   const [audioState, setAudioState] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
   
 
   const playSoundTrack = (): void => {
-    audioState ? audioRef.current?.play() : audioRef.current?.pause();
-    setAudioState(!audioState);    
+    audioState ? audioRef.current.play() : audioRef.current.pause();
+
+    setAudioState(!audioState);
   }
 
   return (
     <>
-      <Button onClick={playSoundTrack}>
-        <VolumeUpIcon id='audioIcon' />
+      <Button id='audioButton' key={`aw_${useId()}`} onClick={playSoundTrack} disableRipple>
+        <VolumeUpIcon key={`aw_${useId()}`} id='audioIcon' sx={{color: audioState ? 'grey' : '#b61588', opacity: audioState ? 0.5 : 1}} />
       </Button>
       <audio key={`aw_${useId()}`} ref={audioRef} src={sound} hidden/>
     </>
