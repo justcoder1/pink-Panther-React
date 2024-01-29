62f732c7957adcc78fb8434ceb6185c1c4e26ef0
Justin Heath <just.coder@outlook.com>
2024-01-29 12:43:03 +0000

    feat: add wikipedia coonection

--- src/presentation/HistoryPage/pages/HistoryPageView.tsx
+++ src/presentation/HistoryPage/pages/HistoryPageView.tsx
@@ -9,7 +9,12 @@ const HistoryPageView: React.FC<
 > = () => {
   const vm = useHistoryViewModel();
 
-  return <AuthenticatedLayoutView>{vm.title}</AuthenticatedLayoutView>;
+  return (
+
+  <AuthenticatedLayoutView>
+      <div>Series number<br />Title<br />Broadcast run<br />Original channel<br />Total # episodes<br />Total # seasons<br /><br />1<br /><br /><i><a href="./The_Pink_Panther_Show" title="The Pink Panther Show">The Pink Panther Show</a></i><br /><br />1969–1979<br /><br /><a href="./NBC" title="NBC">NBC</a> (1969–1978)<br /><br /><a href="./American_Broadcasting_Company" title="American Broadcasting Company">ABC</a> (1978–1979)<br /><br />Three 6-minute shorts + bumpers per half-hour<br /><br />10<br /><br />2<br /><br /><i><a href="./Pink_Panther_and_Sons" title="Pink Panther and Sons">Pink Panther and Sons</a></i><br /><br />1984–1986<br /><br />NBC (1984–1985)<br /><br />ABC (1986)<br /><br />26 episodes<br /><br />1<br /><br />3<br /><br /><i><a href="./The_Pink_Panther_(TV_series)" title="The Pink Panther (TV series)">The Pink Panther</a></i><br /><br />1993–1995<br /><br /><a href="./Broadcast_syndication" title="Broadcast syndication">Syndication</a><br /><br />60 episodes<br /><br />2<br /><br />4<br /><br /><i><a href="./Pink_Panther_and_Pals" title="Pink Panther and Pals">Pink Panther and Pals</a></i><br /><br />2010<br /><br /><a href="./Cartoon_Network" title="Cartoon Network">Cartoon Network</a><br /><br />26 episodes<br /><br />1</div>
+  </AuthenticatedLayoutView>
+  )
 };
 
 export default HistoryPageView;
--- src/presentation/HistoryPage/pages/use-history-page.view-model.ts
+++ src/presentation/HistoryPage/pages/use-history-page.view-model.ts
@@ -1,31 +1,39 @@
-import { ViewModelHook } from '../../../_utils/types/index';
-import { useErrorHandler } from 'react-error-boundary';
-import { useIntl } from 'react-intl';
-
+import { useQuery } from "@tanstack/react-query";
+import { useErrorHandler } from "react-error-boundary";
+import { useIntl } from "react-intl";
+import { ViewModelHook } from "../../../_utils/types/index";
+import { getWikiPediaHistory } from "../_connections/connections";
 
 export interface HistoryViewModel {
-  title: string;  
+  title: string;
 }
 
-const useHistoryViewModel: ViewModelHook<
-HistoryViewModel
-> = () => {
+const useHistoryViewModel: ViewModelHook<HistoryViewModel> = () => {
   const handleError = useErrorHandler();
   const intl = useIntl();
 
+  // API data
+  const { status, data: historyData } = useQuery({
+    queryKey: ["history"],
+    queryFn: getWikiPediaHistory,
+  });
+
+  console.log(status, historyData);
+  
+
   try {
     const title = intl.formatMessage({
-      id: 'title',
-      description: 'page title',
-      defaultMessage: 'History',
+      id: "title",
+      description: "page title",
+      defaultMessage: "History",
     });
 
     return {
       title,
     };
   } catch (error) {
-      handleError(error);
+    handleError(error);
   }
-}
+};
 
 export default useHistoryViewModel;
