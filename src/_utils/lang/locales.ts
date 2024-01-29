002a5160f6cae2605015541dd863890fb8ceeb71
Justin Heath <just.coder@outlook.com>
2024-01-29 18:53:49 +0000

    fix: hook order error message corrected

--- src/_utils/lang/locales.ts
+++ src/_utils/lang/locales.ts
@@ -1,17 +1,6 @@
-import { useIntl } from "react-intl";
-
 // locales supported by the application
 export enum Locales {
   EnGB = 'en-GB',
 }
 
 export const DEFAULT_LOCALE = Locales.EnGB;
-
-export const SetIntlText = (ref: string, textString: string): string => {
-  const intl = useIntl();
-  return intl.formatMessage({
-    id: ref,
-    description: ref,
-    defaultMessage: textString,
-  });
-}
--- src/presentation/AboutPage/pages/AboutPageView.tsx
+++ src/presentation/AboutPage/pages/AboutPageView.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useIntlCommon } from "../../../_utils/lang/intl-common";
+import React from "react";
 import AuthenticatedLayoutView from "../../_AuthenticatedLayoutView/pages/Authenticated/AuthenticatedLayoutView";
 import AboutPageLayout from "../components/AboutPageLayout/AboutPageLayout";
 import useAboutViewModel from "./use-about-page.view-model";
@@ -8,12 +7,6 @@ export interface AboutPageViewProps {}
 
 const AboutPageView: React.FC<React.PropsWithChildren<AboutPageViewProps>> = () => {
   const vm = useAboutViewModel();
-
-  const { siteLabel, aboutLabel } = useIntlCommon();
-
-  useEffect(() => {
-    document.title = `${siteLabel} - ${aboutLabel}`;
-  });
 
   return (
     <AuthenticatedLayoutView>
