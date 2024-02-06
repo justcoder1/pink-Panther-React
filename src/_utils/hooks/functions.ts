fffffdffbd4efe3834bc373fe02bf27d53c80b7c
Justin Heath <just.coder@outlook.com>
2024-02-06 22:49:41 +0000

    chore: removed GITHub pages

--- src/_utils/hooks/functions.ts
+++ src/_utils/hooks/functions.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const noop = () => {};
 
-// export const db_API = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+export const db_API = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 // Temp until I get environments figured out
-export const db_API = axios.create({ baseURL: 'http://18.130.248.203:3200' });
+// export const db_API = axios.create({ baseURL: 'http://18.130.248.203:3200' });
