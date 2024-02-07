88495abd63b9be40820d237119a62576bba96144
Justin Heath <just.coder@outlook.com>
2024-02-07 14:06:02 +0000

    chore: enpoints update to match BE

--- src/presentation/GalleryPage/_connections/connections.ts
+++ src/presentation/GalleryPage/_connections/connections.ts
@@ -2,9 +2,9 @@ import { db_API } from '../../../_utils/hooks/functions';
 import { I_GalleryPicture, I_GalleryVideo } from '../components/GalleryPageLayout/use-gallery-page-layout.view-model';
 
 export const getPictures = async ():Promise<I_GalleryPicture[]> => {
-  return await db_API.get('/picture').then((res) => res.data.data);
+  return await db_API.get('/pictures').then((res) => res.data.data);
 };
 
 export const getVideos = async ():Promise<I_GalleryVideo[]> => {
-  return await db_API.get('/video').then((res) => res.data.data);
+  return await db_API.get('/videos').then((res) => res.data.data);
 };
--- src/presentation/User/_connections/connections.js
+++ src/presentation/User/_connections/connections.js
@@ -1,5 +1,5 @@
 import { db_API } from '../../../_utils/hooks/functions';
 
 export const getUsers = async () => {
-  return await db_API.get('/user').then((res) => res.data.data);
+  return await db_API.get('/users').then((res) => res.data.data);
 };
