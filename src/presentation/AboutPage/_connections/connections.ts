897f9bcde8829d6e217e8305bf3634670e6ab42e
Justin Heath <just.coder@outlook.com>
2024-02-10 21:09:56 +0000

    refactor: http paths to be

--- src/presentation/AboutPage/_connections/connections.ts
+++ src/presentation/AboutPage/_connections/connections.ts
@@ -2,5 +2,5 @@ import { DB_API } from '../../../_utils/hooks/functions';
 import { IntAboutPageLayout } from '../components/AboutPageLayout/use-about-page-layout.view-model';
 
 export const getAbout = async (): Promise<IntAboutPageLayout> => {
-  return DB_API.get('/wikipedia/about').then((res) => res.data.data);
+  return DB_API.get('/pinkpanther/wikipedia/about').then((res) => res.data.data);
 };
--- src/presentation/GalleryPage/_connections/connections.ts
+++ src/presentation/GalleryPage/_connections/connections.ts
@@ -2,9 +2,9 @@ import { DB_API } from '../../../_utils/hooks/functions';
 import { IntGalleryPicture, IntGalleryVideo } from '../components/GalleryPageLayout/use-gallery-page-layout.view-model';
 
 export const getPictures = async (): Promise<IntGalleryPicture[]> => {
-  return DB_API.get('/pictures').then((res) => res.data.data);
+  return DB_API.get('/pinkpanther/pictures').then((res) => res.data.data);
 };
 
 export const getVideos = async (): Promise<IntGalleryVideo[]> => {
-  return DB_API.get('/videos').then((res) => res.data.data);
+  return DB_API.get('/pinkpanther/videos').then((res) => res.data.data);
 };
